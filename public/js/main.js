6702af8adf020d16224ea49191e7e1364fb7654b
Harry Boyes <harry.boyes@myunidays.com>
2017-07-11 12:53:37 +0100

    Rotate the bird

--- public/js/main.js
+++ public/js/main.js
@@ -37,6 +37,9 @@ var mainState = {
 		// Initialise score
 		this.score = 0;
 		this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+
+		// Move the anchor to the left and downward
+		this.bird.anchor.setTo(-0.2, 0.5); 
 	},
 
 	update: function() {
@@ -47,12 +50,25 @@ var mainState = {
 
 		// Use arcade physics to call restartGame if the bird collides with a pipe
 		game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
+
+		// Rotate the bird clockwise until a set point
+		if(this.bird.angle < 20)
+			this.bird.angle += 1;
 	},
 
 	// Make the bird jump 
 	jump: function() {
 	      	// Add a vertical velocity to the bird
 	      	this.bird.body.velocity.y = -350;
+
+		// Create an animation on the bird
+		var animation = game.add.tween(this.bird);
+
+		// Change the angle of the bird to -20° in 100 milliseconds
+		animation.to({angle: -20}, 100);
+
+		// And start the animation
+		animation.start(); 
       	},
 
       	// Restart the game
